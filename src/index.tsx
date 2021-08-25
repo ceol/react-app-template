e20579f21df15b67729ba1de3e5d4ca3835a62db
Patrick Jacobs <ceolwulf@gmail.com>
2021-08-25 16:57:55 -0400

    Add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "./styles"
 
 function App() {
   return (
-    <></>
+    <div className="flex justify-center pt-10">
+      Hello, World!
+    </div>
   )
 }
 
